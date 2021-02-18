56f1c148d04b1e546cfa85a721ffcd6a7abc7006
kiru193 <program922@hotmail.com>
2021-02-18 18:36:55 +0900

    本体移せるか・・・？

---
+++ CGH-ver2.0.0/CGH-ver2.0.0/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main() {
+	printf("hello");
+}
+
+/* opence test
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	Mat image = Mat::zeros(100, 100, CV_8UC3);
+	imshow("", image);
+	waitKey(0);
+}
+*/
+
+/* matplotlib test
+#include <iostream>
+#include <matplotlib-cpp/matplotlibcpp.h>
+#include <vector> 
+using namespace std;
+namespace plt = matplotlibcpp;
+
+int main() {
+	int n = 1000;
+	vector<vector<double>> x, y, z;
+
+	for (double i = -5.0; i < 5.0; i += 1) {
+		vector<double> x_row, y_row, z_row;
+		for (double m = -5.0; m < 5.0; m += 1) {
+			x_row.push_back(i);
+			y_row.push_back(m);
+			z_row.push_back(hypot(i, m));
+		}
+		x.push_back(x_row);
+		y.push_back(y_row);
+		z.push_back(z_row);
+	}
+
+	plt::plot_surface(x, y, z);
+	plt::show();
+
+	return 0;
+}
+*/
