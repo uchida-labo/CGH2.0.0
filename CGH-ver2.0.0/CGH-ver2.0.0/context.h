e284ff2d6941e4307b62cd4848c719e0307b64d2
kiru193 <program922@hotmail.com>
2021-02-21 19:36:58 +0900

    色々追加

---
+++ CGH-ver2.0.0/CGH-ver2.0.0/context.h
@@ -0,0 +1,7 @@
+#pragma once
+constexpr auto PI = 3.14159;
+constexpr auto million = 1000000;
+constexpr auto kilo = 1000;
+constexpr auto mili = 0.001;
+constexpr auto micro = 0.000001;
+constexpr auto nano = 0.000000001;
---
+++ CGH-ver2.0.0/CGH-ver2.0.0/object.cpp
@@ -0,0 +1,7 @@
+#include "object.h"
+
+void Object::P() {
+	point = { {0.0},
+			  {0.0},
+			  {0.0} };
+}
