396cb75e42fe141b4145dbe75b2e20ec498bf054
kiru193 <program922@hotmail.com>
2021-02-24 01:06:11 +0900

    objectに関する追加記述

--- CGH-ver2.0.0/CGH-ver2.0.0/object.cpp
+++ CGH-ver2.0.0/CGH-ver2.0.0/object.cpp
@@ -1,9 +1,38 @@
 #include "object.h"
 
 void Object::P() {
+	point = { {0.0,0.0,0.0} };
+}
 
-	point = { {0.0},
-			  {0.0},
-			  {0.0} };
+void Object::initial_position(double x, double y, double z, vector<vector<double>> object_position) {
 
+	for (int i = 0; i < object_position[0].size(); i++) {
+		point[0][i] = object_position[0][i] + x;
+		point[1][i] = object_position[1][i] + y;
+		point[2][i] = object_position[2][i] + z;
+	}
+}
+
+
+void Media::media_criate(int mediasize_X, int mediasize_Y, int pixcel_pitch,double distance) {
+	point_inf.resize(mediasize_Y);
+	for (int i = 0; i < mediasize_Y; i++) {
+		point_inf[i].resize(mediasize_X);
+		for (int m = 0; m < mediasize_X; m++) {
+			point_inf[i][m].resize(3);
+			point_inf[i][m][0] = (-((double)mediasize_X / 2) + m) * (double)pixcel_pitch * micro;
+			point_inf[i][m][1] = ((double)mediasize_Y / 2 - i) * (double)pixcel_pitch * micro;
+			point_inf[i][m][2] = distance;
+		}
+	}
+}
+
+void Media::initial_position(double x, double y, double z, vector<vector<vector<double>>> point_inf) {
+	for (int i = 0; i < point_inf.size(); i++) {
+		for (int m = 0; m < point_inf[i].size(); m++) {
+			point_inf[i][m][0] = point_inf[i][m][0] + x;
+			point_inf[i][m][1] = point_inf[i][m][1] + y;
+			point_inf[i][m][2] = point_inf[i][m][2] + z;
+		}
+	}
 }
--- CGH-ver2.0.0/CGH-ver2.0.0/object.h
+++ CGH-ver2.0.0/CGH-ver2.0.0/object.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <vector>
+#include "context.h"
+#include"setobject.h"
 
 using namespace std;
 
 class Object {
 public:
 	void P();
+	void initial_position(double x, double y, double z, vector<vector<double>> object_position);
 	vector<vector<double>> point;
+};
+
+class Media {
+public:
+	void media_criate(int mediasize_X, int mediasize_Y, int pixcel_pitch,double distance);
+
+	vector<vector<vector<double>>> point_inf;
+	void initial_position(double x, double y, double z, vector<vector<vector<double>>> point_inf);
 };
