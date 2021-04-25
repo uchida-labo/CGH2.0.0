0d99071a7747ad0dddaad1b892715cb5db0f45e3
Yasutoshi <nyokiyasu.tut@gmail.com>
2021-04-26 05:43:51 +0900

    設定の保存

--- CGH-ver2.0.0/CGH-ver2.0.0/setting.h
+++ CGH-ver2.0.0/CGH-ver2.0.0/setting.h
@@ -2,9 +2,9 @@
 
 class Setting {
 public:
-	int mediasize_X = 1024*3;
-	int mediasize_Y = 768*3;
-	double pixcelpitch = 1.0;
+	int mediasize_X = 2000 / (13.68/12);
+	int mediasize_Y = 2000 / (13.68/12);
+	double pixcelpitch = 13.68 / 12;//1;
 	int HD_width = 1024;
 	int HD_length = 768;
 
@@ -12,5 +12,5 @@ public:
 	double amplitude = 0.5;
 	double apperin_distance = 0.005;
 	double incident_angle = 0;
-	int sepa = 3;//ˆê•Ó“–‚½‚è‚Ì•ªŠ„”
+	int sepa = 1;//ˆê•Ó“–‚½‚è‚Ì•ªŠ„”
 };
