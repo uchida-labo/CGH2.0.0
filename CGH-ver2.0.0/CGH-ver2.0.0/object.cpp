22024713c05e0dfd7d3a0a165bb55989db9b7ff7
Yasutoshi <nyokiyasu.tut@gmail.com>
2021-05-31 09:59:15 +0900

    記述の正規化
    (cherry picked from commit 8e93a73fc00689c881ec9b8662264636a428791f)

--- CGH-ver2.0.0/CGH-ver2.0.0/object.cpp
+++ CGH-ver2.0.0/CGH-ver2.0.0/object.cpp
@@ -53,8 +53,8 @@ void Media::media_criate(int mediasize_X, int mediasize_Y, double pixcel_pitch,d
 		point[i].resize(mediasize_X);
 		for (int m = 0; m < mediasize_X; m++) {
 			point[i][m].resize(3);
-			point[i][m][0] = (-((double)mediasize_X / 2) + m) * (double)pixcel_pitch * micro;
-			point[i][m][1] = ((double)mediasize_Y / 2 - i) * (double)pixcel_pitch * micro;
+			point[i][m][0] = ((mediasize_X / 2.0 - m) * pixcel_pitch * micro);
+			point[i][m][1] = ((mediasize_Y / 2.0 - i) * pixcel_pitch * micro);
 			point[i][m][2] = distance;
 		}
 	}
