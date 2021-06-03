381a90c154d808c3f0200ac4f6df7a1c68f84e2e
nyokiyasu <nyokiyasu.tut@gmail.com>
2021-06-03 15:59:09 +0900

    計算終了時に100%になっていないの気持ち悪かったのでちょっと変更
    (cherry picked from commit d5eb54780e0cfc8cbf4221a0f06f21b9abbdb2d3)

--- CGH-ver2.0.0/CGH-ver2.0.0/culcurate.cpp
+++ CGH-ver2.0.0/CGH-ver2.0.0/culcurate.cpp
@@ -23,7 +23,7 @@ void Culcurate::traditional_method(vector<vector<double>> point_group, vector<ve
 			writing_inf[i][m] = total;
 			total = 0;
 		}
-		printf("working...%d%%\r", (int)((double)i/(double)mediasize_Y*100.0));
+		printf("\rworking...%d%%", (int)((double)(i+1) / (double)mediasize_Y * 100.0));
 	}
 }
 
